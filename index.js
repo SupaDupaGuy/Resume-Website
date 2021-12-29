47ba2c61bbd794900ca40f4cedb06d50a3a1e10c
Markus Elmore <markusje12@gmail.com>
2021-12-29 18:59:23 -0500

    Added the right side panel.
    Added 2 functions to update the element's animation on and
    after hover. Added the filter to the svg images for the
    right side panel.

---
+++ index.js
@@ -0,0 +1,11 @@
+function reverseAnim(idName){
+    element = document.getElementById(idName);
+    element.style.animationDuration = "1s"
+    element.style.animationName = "reverseIcon";
+}
+
+function hoverAnim(idName){
+    element = document.getElementById(idName);
+    element.style.animationDuration = "1s";
+    element.style.animationName = "iconAnim";
+}
