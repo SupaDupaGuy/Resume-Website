e22bca86b233232ee1c06595c08ec901ef139117
Markus Elmore <markusje12@gmail.com>
2021-12-30 08:42:09 -0500

    The bubble plays through the full pop animation and the mouseout event listener will wait for it.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var runningFunc = false;
+
 function reverseAnim(idName){
     element = document.getElementById(idName);
     element.style.animationDuration = "1s"
@@ -10,15 +12,44 @@ function hoverAnim(idName){
     element.style.animationName = "iconAnim";
 }
 
+//only call if another is not running
 function reverseBubbleAnim(idName){
+    console.log('Reverse Hover Bubble');
+    console.log(runningFunc);
     element = document.getElementById(idName);
     element.style.animationDuration = "1s";
     element.style.animationName = "reverseBubbleHoverAnim";
-
 }
 
 function bubbleHoverAnim(idName){
+    console.log('Hover bubble');
     element = document.getElementById(idName);
     element.style.animationDuration = "1s";
     element.style.animationName = "bubbleHoverAnim";
 }
+
+function clicked(idName){
+    bubble = document.getElementById(idName);
+    bubble.onmouseout = (event) => {}
+
+    bubble.style.animationDuration = '1.5s';
+    bubble.style.animationFillMode = 'forwards';
+    bubble.style.animationName = "bubblePop";
+    setTimeout(() => {
+        bubble.onmouseout = (event) => {reverseBubbleAnim('bubble1');};
+        console.log('Mickey mouse clubhouse.');
+    }, 5000);
+    console.log(bubble.onmouseout);
+
+    //bubble.onmouseout = "reverseBubbleAnim('bubble1')";
+    //bubble.onmouseenter = "bubbleHoverAnim('bubble1')";
+    //reverseBubbleAnim('bubble1');
+}
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
