597973ea7129c8ba4f796a2cb508063fb2cf6a21
Markus Elmore <markusje12@gmail.com>
2022-01-03 00:39:34 -0500

    The idle animations resumes after popping or hovering over the bubble.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var runningFunc = false;
 var bubbleQueue = [];
+var bubbleQueue2 = [];
+var bubbleIdleStyle;
 function reverseAnim(idName){
     let element = document.getElementById(idName);
     element.style.animationDuration = "1s"
@@ -19,11 +21,16 @@ function reverseBubbleAnim(idName){
     let element = document.getElementById(idName);
     element.style.animationDuration = "1s";
     element.style.animationName = "reverseBubbleHoverAnim";
+    setTimeout(() => {
+        element.style.animationDuration = '1.5s';
+        element.style.animationName = 'bubbleIdleAnim';
+    }, parseFloat(element.style.animationDuration) * 1000);
 }
 
 function bubbleHoverAnim(idName){
     console.log('Hover bubble');
     let element = document.getElementById(idName);
+    //bubbleIdleStyle = element.style;
     element.style.animationDuration = "1s";
     element.style.animationName = "bubbleHoverAnim";
 }
@@ -33,23 +40,42 @@ function clicked(idName){
     bubble.onmouseout = (event) => {};
     bubble.onmouseenter = (event) => {};
 
+    bubble.style.animationDirection = 'normal';
+    bubble.style.animationIterationCount = 'initial';
     bubble.style.animationDuration = '1.5s';
     bubble.style.animationFillMode = 'forwards';
     bubble.style.animationName = "bubblePop";
     //Add bubble to queue to regrow
     bubbleQueue.push(idName);
+    bubbleQueue2.push(idName);
 
     //time until the bubble regrows
     setTimeout(() => {
         //grow bubble
         let newBubbleId = bubbleQueue.shift();
         let newBubble = document.getElementById(newBubbleId);
+        newBubble.style.animationDirection = 'normal';
+        newBubble.style.animationIterationCount = 'initial';
         newBubble.style.animationFillMode = 'forwards';
         newBubble.style.animationDuration = "1s";
         newBubble.style.animationName = "growBubble";
         newBubble.onmouseout = (event) => {reverseBubbleAnim(newBubbleId);};
         console.log('Mickey mouse clubhouse.');
         newBubble.onmouseenter = (event) => {bubbleHoverAnim(newBubbleId);};
+        //Readd Idle animation
+        //newBubble.style.animationName = 'bubbleIdleAnim';
+        
+        //Readd the idle animation after the length of the regrow animation duration
+        console.log(parseFloat(newBubble.style.animationDuration) * 1000);
+        setTimeout(() => {
+            let newBubbleID2 = bubbleQueue2.shift();
+            let newBubble2 = document.getElementById(newBubbleID2);
+            newBubble2.style.animationIterationCount = 'infinite';
+            newBubble2.style.animationDirection = 'alternate';
+            newBubble2.style.animationDuration = '1.5s';
+            newBubble2.style.animationName = 'bubbleIdleAnim';
+        }, parseFloat(newBubble.style.animationDuration) * 1000);
+
     }, 5000);
     console.log(bubble.onmouseout);
 
